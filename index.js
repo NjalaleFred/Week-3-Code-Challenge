aee3355d0f2286c3e42b6e3829bc4537cb1e3e88
Njalale <fnjalale@gmail.com>
2023-03-31 18:12:16 +0300

    Core Deliverables

---
+++ index.js
@@ -0,0 +1,104 @@
+//Declare variables that I will use oftenly.
+const baseURL = 'http://localhost:3000/films';
+
+const headers = {
+    'Content-Type' : 'apllication/jaon',
+};
+
+//Load your content to the dome
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
+    
+    fetchFilm1();
+    fetchFilms();
+})
+
+//Describe a function fetchFilms1 that will fetch the first film.
+//In it is another function that renders the first character.
+
+function fetchFilm1(){
+    fetch(`${baseURL}/1`, {
+        method : 'GET',
+        headers,
+    })
+    .then((resp) => resp.json())
+    .then(renderFilm1)
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+let f1 = document.querySelector('#f1_details')
+
+//The function that renders the film is described. 
+function renderFilm1(film){
+    const card = document.createElement('div')
+    card.className = "card"
+
+    card.innerHTML = `
+    <h2> ${film.title} </h2>
+    <p> ${film.description} </p>
+    <img id="image" src ='${film.poster}' ></img>
+    <p> Runtime : ${film.runtime} min </p>
+    <p> Showtime : ${film.showtime} </p>
+    `
+//Available tickets is calculated.
+   let AvailableTickets = `${film.capacity}` - `${film.tickets_sold}`
+
+   const p = document.createElement('p')
+   p.textContent =  `Available tickets : ${AvailableTickets}`
+
+   const btn = document.createElement('button')
+   btn.textContent = 'Buy Ticket'
+
+   //Here the btn receives a callback function that is executed when it is clicked.
+    btn.addEventListener('click', function(){
+        if (AvailableTickets > 0){
+            AvailableTickets --;
+            if (AvailableTickets ===0){
+                btn.innerHTML = 'Sold Out';
+                btn.disabled = true;
+            } else {
+                btn.innerHTML = 'Buy Ticket'
+            }
+        }
+        p.textContent =  `Available tickets : ${AvailableTickets}`
+    })
+   
+    //Append the created elements.
+    f1.append(card,p, btn,);
+}
+
+//Another function that fetches all films is described that will show a list of the films.
+//It received a function that renders the films.
+function fetchFilms(){
+    fetch(`${baseURL}`, {
+        method : 'GET',
+        headers
+    })
+    .then((resp) => resp.json())
+    .then(renderFilms)
+    .catch((error) => {
+        console.log(error);
+    })
+}
+
+//The function that renders the films is described.
+function renderFilms(film){
+
+    const allFilms = document.querySelector('#all_films')
+    allFilms.innerHTML = '<h2> MENU </h2>'
+
+//A forEach loop is used to loop over the the films and then renders the film titles.
+   film.forEach((film) => {
+    const list = document.createElement('div');
+    list.className = "list"
+    list.innerHTML = `
+        <ol> ${film.id} : ${film.title} </ol>
+    `
+
+//Created elements are then appended.
+    allFilms.append(list);
+   });
+}
+
